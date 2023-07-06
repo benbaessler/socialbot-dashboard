faa96e5861bada30b3718a5b008c48cf6bba5479
benbaessler <mail@benbaessler.com>
2023-07-06 15:07:47 +0100

    chore: fetch guilds

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
+import { useState, useEffect, use } from "react";
+
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
 
 import { useSession, signIn } from "next-auth/react";
+import { getOwnedGuilds } from "./getGuilds";
 
 export default function Home() {
   const { data: session, status } = useSession();
+
+  const [guilds, setGuilds] = useState();
+
+  useEffect(() => {
+    if (status == "authenticated") {
+      getOwnedGuilds((session as any)?.accessToken).then((guilds) => {
+        setGuilds(guilds);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (status == "unauthenticated") {
     signIn("discord");
@@ -18,5 +32,4 @@ export default function Home() {
       </main>
     );
   }
-
 }
