20e6fee562609f1a56446404737c7c22d8607b9e
benbaessler <mail@benbaessler.com>
2023-07-06 19:47:54 +0100

    feat: add schema interfaces

--- types/index.ts
+++ types/index.ts
@@ -7,3 +7,25 @@ export interface Guild {
   permissions: number;
   permissions_new: string;
 }
+
+export interface IStats extends Document {
+  guildId: string;
+  joinedAt: Date | null;
+  postsPosted: number;
+  commentsPosted: number;
+  mirrorsPosted: number;
+  collectsPosted: number;
+  commandsUsed: number;
+}
+
+export interface IInstance extends Document {
+  guildId: string;
+  channelId: string;
+  handle: string;
+  profileId: string;
+  ownedBy: string;
+  includeMirrors: boolean;
+  includeInteractions: boolean;
+  mention: boolean;
+  webhookUrl: string;
+}
