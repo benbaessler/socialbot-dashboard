3a4d2ff41936de1efac8724644bf52729ee4c6de
benbaessler <mail@benbaessler.com>
2023-07-06 15:56:05 +0100

    chore: fix navbar positioning

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -3,11 +3,15 @@ import Image from "next/image";
 import LogoutIcon from "@mui/icons-material/Logout";
 import LogoImage from "@/assets/logo.png";
 
+import { Select, Button } from "@chakra-ui/react";
+
 import { useSession, signOut } from "next-auth/react";
+import { useState } from "react";
 
-const Navbar = () => {
-
+const Navbar = ({ guilds }: any) => {
   const { data: session, status } = useSession();
+
+  const [guild, setGuild] = useState(guilds[0]);
 
   return (
     <nav className="flex w-full p-4 justify-between">
@@ -24,15 +28,24 @@ const Navbar = () => {
         </div>
       </div>
 
-      <button 
-        className="flex bg-gray-700 hover:bg-gray-800 text-white py-2.5 px-5 rounded-lg"
-        onClick={() => signOut()}
-      >
-        <div className="pr-2">
-          <LogoutIcon fontSize="small" />
-        </div>
-        Logout
-      </button>
+      <div className="flex gap-3">
+        <Select defaultValue={guilds[0].name}>
+          {guilds?.map((guild: any) => (
+            <option key={guild.id} value={guild.id}>
+              {guild.name}
+            </option>
+          ))}
+        </Select>
+        <button
+          className="flex bg-gray-800 hover:bg-gray-900 text-white py-2 px-5 rounded-md"
+          onClick={() => signOut()}
+        >
+          <div className="pr-2">
+            <LogoutIcon fontSize="small" />
+          </div>
+          Logout
+        </button>
+      </div>
     </nav>
   );
 };
