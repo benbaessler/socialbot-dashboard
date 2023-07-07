024a5a858d5a7bdce18d3facff5aca04f36cc080
benbaessler <mail@benbaessler.com>
2023-07-07 07:50:59 +0100

    chore: add error message

--- pages/components/AddFeed.tsx
+++ pages/components/AddFeed.tsx
@@ -13,7 +13,7 @@ import { useState, useContext } from "react";
 import { GuildContext } from "@/context/Guild";
 import { ChannelsContext } from "@/context/Channels";
 import { FeedsContext } from "@/context/Feeds";
-import { hexToNumber, getPictureUrl } from "@/utils";
+import { hexToNumber, getPictureUrl, parseHandle } from "@/utils";
 import { IFeed } from "@/types";
 
 interface Props {
@@ -32,6 +32,7 @@ const AddFeed = ({ className }: Props) => {
   const { feeds, setFeeds } = useContext(FeedsContext);
 
   const [invalidHandle, setInvalidHandle] = useState(false);
+  const [error, setError] = useState("");
 
   const [channel, setChannel] = useState<any>();
   const [handle, setHandle] = useState("");
@@ -51,6 +52,14 @@ const AddFeed = ({ className }: Props) => {
   };
 
   const handleSubmit = async () => {
+    const duplicate = feeds.some(
+      (feed: IFeed) =>
+        feed.handle == parseHandle(handle) && feed.channelId == channel.id
+    );
+
+    if (duplicate) return setError("This feed already exists.");
+    setError("");
+
     const res = await fetch(`/api/lens/getProfile?handle=${handle}`);
     const profile = await res.json();
 
@@ -102,90 +111,97 @@ const AddFeed = ({ className }: Props) => {
   };
 
   return (
-    <div className={`${className} flex-col space-y-5 bg-slate-800 rounded-xl`}>
-      <div>
-        <div className="flex items-center mb-2">
-          <span className="font-semibold mr-2">Handle</span>
-          <span className="text-sm text-slate-400">
-            The Lens profile handle to add.
-          </span>
+    <div
+      className={`${className} flex-col justify-between bg-slate-800 rounded-xl`}
+    >
+      <div className="space-y-6">
+        <div>
+          <div className="flex items-center mb-2">
+            <span className="font-semibold mr-2">Handle</span>
+            <span className="text-sm text-slate-400">
+              The Lens profile handle to add.
+            </span>
+          </div>
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <AtSignIcon />
+            </InputLeftElement>
+            <Input
+              value={handle}
+              onChange={handleInputChange}
+              isInvalid={invalidHandle}
+            />
+          </InputGroup>
         </div>
-        <InputGroup>
-          <InputLeftElement pointerEvents="none">
-            <AtSignIcon />
-          </InputLeftElement>
-          <Input
-            value={handle}
-            onChange={handleInputChange}
-            isInvalid={invalidHandle}
-          />
-        </InputGroup>
-      </div>
-      <div>
-        <div className="flex items-center mb-2">
-          <span className="font-semibold mr-2">Channel</span>
-          <span className="text-sm text-slate-400">
-            The channel to send updates to.
-          </span>
+        <div>
+          <div className="flex items-center mb-2">
+            <span className="font-semibold mr-2">Channel</span>
+            <span className="text-sm text-slate-400">
+              The channel to send updates to.
+            </span>
+          </div>
+          <Select
+            placeholder="Select a channel"
+            value={channel?.name || "Select a channel"}
+            onChange={(e) => {
+              setChannel(
+                channels.find((channel: any) => channel.name == e.target.value)
+              );
+            }}
+          >
+            {channels.map((channel: any) => (
+              <option key={channel.id} value={channel.name}>
+                #{channel.name}
+              </option>
+            ))}
+          </Select>
         </div>
-        <Select
-          placeholder="Select a channel"
-          value={channel?.name || "Select a channel"}
-          onChange={(e) => {
-            setChannel(
-              channels.find((channel: any) => channel.name == e.target.value)
-            );
-          }}
-        >
-          {channels.map((channel: any) => (
-            <option key={channel.id} value={channel.name}>
-              #{channel.name}
-            </option>
-          ))}
-        </Select>
-      </div>
-      <div>
-        <div className="flex items-center mb-2">
-          <span className="font-semibold mr-2">Options</span>
-          <span className="text-sm text-slate-400">
-            Specify monitor options.
-          </span>
+        <div>
+          <div className="flex items-center mb-2">
+            <span className="font-semibold mr-2">Options</span>
+            <span className="text-sm text-slate-400">
+              Specify monitor options.
+            </span>
+          </div>
+          <CheckboxGroup colorScheme="blue">
+            <Stack direction={["column"]}>
+              <Checkbox
+                value="mirrors"
+                isChecked={options.mirrors}
+                onChange={() => handleCheckboxChange("mirrors")}
+              >
+                Include Mirrors
+              </Checkbox>
+              <Checkbox
+                value="collects"
+                isChecked={options.collects}
+                onChange={() => handleCheckboxChange("collects")}
+              >
+                Include Collects
+              </Checkbox>
+              <Checkbox
+                value="mentions"
+                isChecked={options.mentions}
+                onChange={() => handleCheckboxChange("mentions")}
+              >
+                Mention everyone
+              </Checkbox>
+            </Stack>
+          </CheckboxGroup>
         </div>
-        <CheckboxGroup colorScheme="blue">
-          <Stack direction={["column"]}>
-            <Checkbox
-              value="mirrors"
-              isChecked={options.mirrors}
-              onChange={() => handleCheckboxChange("mirrors")}
-            >
-              Include Mirrors
-            </Checkbox>
-            <Checkbox
-              value="collects"
-              isChecked={options.collects}
-              onChange={() => handleCheckboxChange("collects")}
-            >
-              Include Collects
-            </Checkbox>
-            <Checkbox
-              value="mentions"
-              isChecked={options.mentions}
-              onChange={() => handleCheckboxChange("mentions")}
-            >
-              Mention everyone
-            </Checkbox>
-          </Stack>
-        </CheckboxGroup>
       </div>
 
-      <Button
-        colorScheme="green"
-        variant="solid"
-        onClick={handleSubmit}
-        isDisabled={!handle || !channel}
-      >
-        Start monitor
-      </Button>
+      <div className="flex flex-col">
+        <span className="text-sm text-red-800 my-3">{error}</span>
+        <Button
+          colorScheme="green"
+          variant="solid"
+          onClick={handleSubmit}
+          isDisabled={!handle || !channel}
+        >
+          Start monitor
+        </Button>
+      </div>
     </div>
   );
 };
