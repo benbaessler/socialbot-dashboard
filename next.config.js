0e400b598476c5b434312687c361b941130f0019
benbaessler <mail@benbaessler.com>
2023-07-07 08:27:59 +0100

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    esmExternals: "loose", 
+    serverComponentsExternalPackages: ["mongoose"], 
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
-      }
-    ]
-  }
-}
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
