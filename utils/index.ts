1fe84234472edb7e25284b56b5a6ce91603b0e77
benbaessler <mail@benbaessler.com>
2023-07-07 05:11:10 +0100

    chore: update utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,32 @@
-export const getOwnedGuilds = async (token: string) => {
-  const res = await fetch("https://discord.com/api/users/@me/guilds", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const guilds = await res.json();
+import mongoose from "mongoose";
+import { LensClient, production } from "@lens-protocol/client";
+require("dotenv").config();
 
-  return guilds.filter((guild: any) => guild.owner);
+export const connectDB = async () => {
+  await mongoose.connect(process.env.MONGODB_URI!);
+  console.log("Connected to DB");
+};
+
+export const lensClient = new LensClient({
+  environment: production,
+});
+
+export const numberToHex = (num: number) => {
+  let hexValue = num.toString(16);
+
+  if (hexValue.length % 2 !== 0) {
+    hexValue = "0" + hexValue;
+  }
+
+  return `0x${hexValue}`;
+};
+
+export const hexToNumber = (hex: string) => parseInt(hex, 16).toString();
+
+
+export const parseHandle = (input: string): string => {
+  if (!input.endsWith(".lens") && input != "lensprotocol") {
+    return input + ".lens";
+  }
+  return input;
 };
