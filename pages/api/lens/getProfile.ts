b31ba18fade82939f952686b1feaa25c86c620c8
benbaessler <mail@benbaessler.com>
2023-07-07 05:09:10 +0100

    feat: lens profile endpoint

---
+++ pages/api/lens/getProfile.ts
@@ -0,0 +1,18 @@
+import { lensClient, parseHandle } from "@/utils";
+
+// @ts-ignore
+const handler = async (req, res) => {
+  const { handle } = req.query;
+
+  try {
+    const profile = await lensClient.profile.fetch({
+      handle: parseHandle(handle),
+    });
+    res.status(200).json(profile);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error fetching profile" });
+  }
+};
+
+export default handler;
